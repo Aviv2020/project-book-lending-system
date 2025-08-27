b95a5439c38eff84a9ed22750d397da6567070e2
Aviv2020 <avivbenamar@gmail.com>
2025-08-27 16:25:57 +0300

    Delete book-lending-system-main/server/test.js

--- book-lending-system-main/server/test.js
+++
@@ -1,6 +0,0 @@
-const { logAction } = require("./sheetLogger");
-
-(async () => {
-  const now = new Date().toLocaleString("he-IL");
-  await logAction("333599611", "אביב בן עמר", "מקראה בספרות", "בדיקת חיבור", now);
-})();
