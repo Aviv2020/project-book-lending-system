df962ff37687ebdd5b8f5a44d8474ec159581513
shir <motieishira@gmail.com>
2025-08-31 15:38:30 +0300

    trying to see the new commit

--- book-lending-system-main/server/models/Address.js
+++ book-lending-system-main/server/models/Address.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
 const addressSchema = new mongoose.Schema({
-  id: {type: Number, required: true},
   title: { type: String }, // שם הכתובת
-  lat: { type: Number }, //קורדינטת רוחב
-  lng: { type: Number } //קורדינטת אורך 
+  lat: { type: Number, required: true }, //קורדינטת רוחב
+  lng: { type: Number, required: true } //קורדינטת אורך 
 }, {
   timestamps: true // מוסיף createdAt, updatedAt
 });
