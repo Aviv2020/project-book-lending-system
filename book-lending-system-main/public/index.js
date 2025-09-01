27bd61be24d876d385bb892280d32dc7119b8c2b
shir <motieishira@gmail.com>
2025-09-01 17:37:00 +0300

    did the video's base

--- book-lending-system-main/public/index.js
+++ book-lending-system-main/public/index.js
@@ -5525,3 +5525,13 @@ async function deleteAddress(id) {
   });
   loadSearchHistory();
 }
+
+
+
+function openPopup() {
+  document.getElementById("videoPopup").style.display = "flex";
+}
+
+function closePopup() {
+  document.getElementById("videoPopup").style.display = "none";
+}
